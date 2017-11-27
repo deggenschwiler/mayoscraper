262b21c0cc197e6c1b94d5368d907588bf49141a
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2017-11-27 16:49:56 +0000

    fixed?

--- mayoscraper.py
+++ mayoscraper.py
@@ -7,32 +7,6 @@ import datetime
 from bs4 import BeautifulSoup as bs
 import csv
 
-
-page = ur.urlopen('https://www.instagram.com/shortcutsplus/')
-soup = bs(page.read(),"html.parser")
-body = soup.find('body',{'class':''})
-script = body.find('script',{'type':'text/javascript'})
-data = json.loads(script.text.replace('window._sharedData = ', '')[:-1])
-a = []
-
-for i in range(1):
-	f = open("shortcutsplus1127_1hour.csv", 'a', newline='')
-	writer = csv.writer(f)
-	postid = (data['entry_data']['ProfilePage'][0]['user']['media']['nodes'][i]['code'])
-	postDate = (data['entry_data']['ProfilePage'][0]['user']['media']['nodes'][i]['date'])
-	likes = (data['entry_data']['ProfilePage'][0]['user']['media']['nodes'][i]['likes']['count'])
-	comments = (data['entry_data']['ProfilePage'][0]['user']['media']['nodes'][i]['comments']['count'])
-	followers = (data['entry_data']['ProfilePage'][0]['user']['followed_by']['count'])
-	follows = (data['entry_data']['ProfilePage'][0]['user']['follows']['count'])
-	# a.append((postid, postDate, likes, comments))
-
-	b = (postid, likes, comments)
-writer.writerow(((datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), followers, follows,  postid, likes, comments)))
-print((datetime.datetime.now().strftime("%H:%M:%S %d/%m/%Y") ,followers, follows, postid, likes, comments))
-f.close()
-
-
-
 def IG_job():
 	page = ur.urlopen('https://www.instagram.com/shortcutsplus/')
 	soup = bs(page.read(),"html.parser")
@@ -41,9 +15,7 @@ def IG_job():
 	data = json.loads(script.text.replace('window._sharedData = ', '')[:-1])
 	a = []
 
-	for i in range(1):
-		f = open("shortcutsplus1127_1hour.csv", 'a', newline='')
-		writer = csv.writer(f)
+	for i in range(6):
 		postid = (data['entry_data']['ProfilePage'][0]['user']['media']['nodes'][i]['code'])
 		postDate = (data['entry_data']['ProfilePage'][0]['user']['media']['nodes'][i]['date'])
 		likes = (data['entry_data']['ProfilePage'][0]['user']['media']['nodes'][i]['likes']['count'])
@@ -52,11 +24,19 @@ def IG_job():
 		follows = (data['entry_data']['ProfilePage'][0]['user']['follows']['count'])
 		# a.append((postid, postDate, likes, comments))
 
-		b = (postid, likes, comments)
-	writer.writerow(((datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), followers, follows,  postid, likes, comments)))
-	print((datetime.datetime.now().strftime("%H:%M:%S %d/%m/%Y") ,followers, follows, postid, likes, comments))
+		#b = (postid, likes, comments)
+		if (i < 5 or i > 10):
+			if (i < 1):
+				a += (((datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), followers, follows,  postid, likes, comments)))
+			else:
+				a += (likes, comments)
+	print(a)
+	f = open("shortcutsplus1127_1hour.csv", 'a', newline='')
+	writer = csv.writer(f)
+
+	writer.writerow(a)
 	f.close()
 
 scheduler = BlockingScheduler()
-scheduler.add_job(IG_job, 'interval', hours=1)
+scheduler.add_job(IG_job, 'interval', seconds=4)
 scheduler.start()
