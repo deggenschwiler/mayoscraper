eeb37bdb8e96972b3f30403465731f11dc044dff
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2017-12-12 09:48:54 +0000

    general price watcher

---
+++ timetobuy.py
@@ -0,0 +1,46 @@
+import csv
+import requests
+import json
+from bs4 import BeautifulSoup as bs
+import operator
+import sys
+import urllib
+import urllib.request as ur
+import json
+import re
+import datetime
+import csv
+import time
+from os import system
+import random
+import math
+from fake_useragent import UserAgent
+
+#set user agent so they don't know this is a bot
+ua = UserAgent()
+
+#function to check price is the best and reasonable
+def bestprice():
+    #set random timout to not annoy the site owner
+    timeout = random.randrange(240,720,1)
+    #print how long it's going to wait this round
+    print("-------------- " + str(timeout) + "s --------------")
+
+    # check price is reasonable
+    myprice = 12000.0
+    theirprice = float(jobject['response'][1]['price_unit'])
+    # if it's way lower - then anounce that
+    if (theirprice < myprice):
+        announcement = "Time to buy, price dropped to £" + str(theirprice)
+        print(announcement)
+        system("say " + announcement)
+    time.sleep(timeout)
+
+# Do until I say stop
+while(True):
+    # set target api and fake user agent
+    response = requests.get('https://bitbargain.co.uk/api/buy', headers={'User-Agent': ua.random})
+    # grab the data as an object
+    jobject = json.loads(response.content.decode('utf8'))
+    # run the price checking function
+    bestprice()
